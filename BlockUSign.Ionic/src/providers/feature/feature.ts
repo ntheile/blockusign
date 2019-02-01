440f530ea24d9e679e351c1a9ef61c5d5adc521b
nick theile <Nick@nicksmbp.local>
2019-01-31 23:52:26 -0600

    uncomment feature post

--- BlockUSign.Ionic/src/providers/feature/feature.ts
+++ BlockUSign.Ionic/src/providers/feature/feature.ts
@@ -58,7 +58,7 @@ export class FeatureProvider {
   async putMyFeatures(){
     let resp = null;
     try{
-      //resp = blockstack.putFile('features.json', JSON.stringify(this.features), {encrypt: false});
+      resp = blockstack.putFile('features.json', JSON.stringify(this.features), {encrypt: false});
     } catch(e){
       return null;
     }
